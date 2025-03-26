11ce8d22ffb5c774c46f0fb1ccc4ca0014dbcf91
Tyga-x <wishvajay02@gmail.com>
2025-03-26 14:30:17 +0530

    Update cap-uninstall.sh

--- cap-uninstall.sh
+++ cap-uninstall.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+echo "Uninstalling Bandwidth Limiter..."
+
+# Remove the global command
+sudo rm -f /usr/local/bin/cap-vps
+
+# Remove the main script directory
+sudo rm -rf /usr/local/bin/cap-vps/
+
+# Reset UFW rules
+sudo ufw reset
+
+# Remove vnstat data (optional)
+sudo apt remove -y vnstat jq
+
+echo "Uninstallation complete!"
